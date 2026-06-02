472af3b1c51af3da5dd58f8d3d6f5094864f090e
Leo <Leo1987tw@gmail.com>
2026-06-02 23:02:36 +0800

    add flipping card

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>隨機英文單字卡</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <header></header>
+    <main>
+        <?php include "./vocabuary.php;"?>
+    </main>
+    <footer></footer>
+</body>
+</html>
